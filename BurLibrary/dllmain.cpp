275948ef70f0f0d8915819b3ea68364a5edd9ba4
OlehBur <125184382+OlehBur@users.noreply.github.com>
2023-09-11 20:25:32 +0300

    final version of the library (dll) with fixed errors and calculations

--- BurLibrary/dllmain.cpp
+++ BurLibrary/dllmain.cpp
@@ -18,94 +18,115 @@ BOOL APIENTRY DllMain( HMODULE hModule,
     }
     return TRUE;
 }
+//extern "C" __declspec(dllexport) float __cdecl BurFunction(float xValue)
+//{
+//    float Y;
+//    float One = 1.0f, Two = 2.0f, Three = 3.0f, Four = 4.0f, Ten = 10.0f;
+//    float M[4] = { 1.0f, 2.0f, 3.0f, 4.0f }; // масив елементів
+//    int n = 4; // n - кількість елементів масиву
+//
+//    // Обчислюємо суму елементів масиву
+//    float sum = 0;
+//    for (int i = 0; i < n; i++)
+//        sum += M[i];
+//
+//    // Виконуємо різні дії в залежності від значення xValue
+//    if (xValue == One)
+//        Y = One + Two;
+//    else if (xValue == Two)
+//        Y = Two * sum + Ten * xValue * xValue * sum;
+//    else if (xValue >= Four)
+//        Y = Three * Four;
+//    else
+//        Y = std::numeric_limits<float>::quiet_NaN(); // повертаємо NaN    //#include <limits>
+//
+//    return Y;
+//}
 
 extern "C" __declspec(dllexport) float __cdecl BurFunction(float xValue)
 {
-    float Y;
-    float One, Two, Three, Four, Ten, sum;
-    float M[4] = { 1.0f, 2.0f, 3.0f, 4.0f }; // масив елементів
-    int i, n = 4; // n - кількість елементів масиву
+    //ініц змінних
+    float Y { 0.0f };//результ
+    float One = 1.0f, Two = 2.0f, Three = 3.0f, Four = 4.0f, Ten = 10.0f;//доп. змінні для обчислень
+    float sum { 0.0f };//змінна суми
+    float M[4] = { 1.0f, 2.0f, 3.0f, 4.0f }; // масив певних чисел
+    int n = 4; // n - к-сть ел. масиву
 
     _asm
     {
-        FINIT; // ініціалізація регістра FPU
-        FLD1; // завантажуємо 1 в ST(0)
-        FSTP dword ptr[One]; // зберігаємо значення ST(0) в One
-        FLD1; // завантажуємо 1 в ST(0)
-        FADD dword ptr[One]; // додаємо One до ST(0)
-        FSTP dword ptr[Two]; // зберігаємо значення ST(0) в Two
-        FLD1; // завантажуємо 1 в ST(0)
-        FADD dword ptr[Two]; // додаємо Two до ST(0)
-        FSTP dword ptr[Three]; // зберігаємо значення ST(0) в Three
-        FLD1; // завантажуємо 1 в ST(0)
-        FADD dword ptr[Three]; // додаємо Three до ST(0)
-        FSTP dword ptr[Four]; // зберігаємо значення ST(0) в Four
-        FLD1; // завантажуємо 1 в ST(0)
-        FADD dword ptr[Four]; // додаємо Four до ST(0)
-        FADD dword ptr[Four]; // додаємо Four до ST(0)
-        FSTP dword ptr[Ten]; // зберігаємо значення ST(0) в Ten
-
-        xor ecx, ecx; // обнуляємо лічильник
-        fldz; // завантажуємо 0 в ST(0)
+        xor ecx, ecx; // занулення коунтера
+        fldz; // завантаження 0 в фпю (стекс - ст(0))
 
     SumLoop:
-        fadd dword ptr[M + ecx * 4]; // додаємо поточний елемент масиву до ST(0)
-        inc ecx; // інкрементуємо лічильник
-        cmp ecx, n; // порівнюємо значення лічильника з кількістю елементів
-        jl SumLoop; // якщо лічильник менше кількості елементів, повертаємось на початок цикла
+        fadd dword ptr[M + ecx * 4]; // додавання курентного елемента масива в ст.
+        inc ecx; // інкрементим коунтер
+        cmp ecx, n; // зіставлення значення коунтера з к-стю елементів масиву
+        jl SumLoop; // при умові коунтера - менше к-сті елементів, стрибаємо на початок цикла
 
-        fstp dword ptr[sum]; // зберігаємо суму елементів масиву
+        fstp dword ptr[sum]; // сейв суми елементів масиву
 
-        fld xValue; // завантажуємо xValue в регістр st(0)
-
-        fld st(0);
-        fcomp dword ptr[One];
-        fstsw ax;
-        sahf;
-        je Case1;
+        fld xValue; // завантаж. xValue в регістр стеку
 
         fld st(0);
-        fcomp dword ptr[Two];
+        fcomp dword ptr[One];//зіставляння Х та 1
         fstsw ax;
         sahf;
-        je Case2;
+        je Case1; //при Х=1, стрибаємо на Вип.1
 
-    Case1:
-        fld dword ptr[One];
-        fadd dword ptr[Two];
-        fstp Y;
-        jmp End;
+        fld st(0);
+        fcomp dword ptr[Two];//Х та 2
+        fstsw ax;
+        sahf;
+        je Case2;//при Х=2 - стрибаємо на Вип.2
 
-    Case2:
-        fld xValue;
-        fmul st(0), st(0);
-        fmul dword ptr[Ten];
-        fmul dword ptr[sum];
+        fld st(0);//Х та 4
+        fcomp dword ptr[Four];
+        fstsw ax;
+        sahf;
+        jae Case4;//при Х>=4 -  пригаємо на Вип.4
 
-        fld dword ptr[sum];
-        fmul dword ptr[Two];
+        //в усіх інших випадках де не спрацювали провірки
+    DefaultCase://при Х=3 та Х<=0
+        fldz;//занул регістра стеку
+        fdiv st(0), st(0); //ділимо 0 на 0 (отримаємо НаН)
+        fstp Y;//сейвимо отримане значення в змінну
 
-        faddp st(1), st(0);
+        jmp End;//ліваєм на кінець функи
 
-        fstp Y;
-        jmp End;
+    Case1://Х=1
+        fld dword ptr[One]; //закидуємо значення 1 в стек
+        fadd dword ptr[Two];//додаємо до нього 2
+        fstp Y;//отримуєм на виході 3 котре зберігаємо в У
 
-    DefaultCase:
-        fldz;
-        fdiv st(0), st(0);
-        fstp Y;
+        jmp End;//вистрибуємо в кінець функи
 
-        jmp End;
+    Case2://Х=2
+        fld xValue;//закид в стек Х
+        fmul st(0), st(0);//робимо квадрат Х
+        fmul dword ptr[Ten];//квадрат *10
+        fmul dword ptr[sum];//* сума ел. масиву
 
-    Case4:
-        fld dword ptr[Four];
-        fadd dword ptr[Three];
-        fstp Y;
+        fld dword ptr[sum];//закид суму в фпю стеку.-(0)
+        fmul dword ptr[Two];//*2
 
-    End:
+        faddp st(1), st(0);//додаємо ці два значення в стеку
+
+        fstp Y;//сейвим результ в У
+
+        jmp End;//виприг
+
+    Case4://х>=4
+        fld dword ptr[Three];//3 в стек
+        fmul dword ptr[Four];//*4
+        fstp Y;//отримані 12 кидаємо в У
+
+    End://інші х
     }
-    return Y;
+
+    return Y; // повертаємо результат функи
 }
+
+
 
 
 //extern "C" __declspec(dllexport) float __cdecl BurFunction(float xValue)
